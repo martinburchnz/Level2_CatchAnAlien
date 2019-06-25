82dcd5c19635361f5b986fbb8e7708fc16651359
Martin Burch <MartinB@mmc.school.nz>
2019-06-25 21:02:54 +1200

    Changed panel colour on home computer

--- Level2_CatchAnAlien/Level2_CatchAnAlien/Form1.Designer.cs
+++ Level2_CatchAnAlien/Level2_CatchAnAlien/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // pnlGame
             // 
-            this.pnlGame.BackColor = System.Drawing.Color.Silver;
+            this.pnlGame.BackColor = System.Drawing.Color.Beige;
             this.pnlGame.Location = new System.Drawing.Point(30, 140);
             this.pnlGame.Name = "pnlGame";
             this.pnlGame.Size = new System.Drawing.Size(300, 300);
